59bdfb38891f143116d8fb34adf6e484588a0660
JeffcoatDesign <98057241+JeffcoatDesign@users.noreply.github.com>
2022-12-12 12:02:28 -0700

    v0.2.9 Boxing Ring, Gameplay Changes, and Visual Changes

--- Ham-fisted/Assets/Scripts/FloatingNameTag.cs
+++ Ham-fisted/Assets/Scripts/FloatingNameTag.cs
@@ -10,6 +10,9 @@ public class FloatingNameTag : MonoBehaviour
 
     void Update ()
     {
+        if (ball == null)
+            return;
+
         transform.parent.position = ball.position;
         transform.LookAt(Camera.main.transform.position);
         transform.Rotate(0, 180, 0);
